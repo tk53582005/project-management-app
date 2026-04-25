1d57f1f1d86a2972fbaf659680179726820c8b6d
Kazuhiro <tk53582005@yahoo.co.jp>
2026-04-25 20:01:59 +0900

    Fix Cognito callback URL cleanup

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { AuthProvider } from 'react-oidc-context'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { AuthProvider } from "react-oidc-context";
+import "./index.css";
+import App from "./App.jsx";
 
 const cognitoAuthConfig = {
-  authority: 'https://cognito-idp.ap-northeast-1.amazonaws.com/ap-northeast-1_LF0Frqj4b',
-  client_id: '4vrs82vcgc93shqql5bdngdrfd',
+  authority:
+    "https://cognito-idp.ap-northeast-1.amazonaws.com/ap-northeast-1_LF0Frqj4b",
+  client_id: "4vrs82vcgc93shqql5bdngdrfd",
   redirect_uri: window.location.origin,
-  response_type: 'code',
-  scope: 'email openid phone',
-}
+  response_type: "code",
+  scope: "email openid phone",
 
-createRoot(document.getElementById('root')).render(
+  onSigninCallback: () => {
+    window.history.replaceState({}, document.title, window.location.pathname);
+  },
+};
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider {...cognitoAuthConfig}>
       <App />
     </AuthProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
